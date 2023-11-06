e9ff94126ea3f3a8ecdc0bc2f4cb159d86364b44
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-11-06 10:23:49 +0900

    [level unrated] Title: 편지, Time: 0.11 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        String[] arr = message.split("");
+       
+        return arr.length * 2;
+    }
+}
