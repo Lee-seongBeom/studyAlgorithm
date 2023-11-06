1f878667fbb6d315fe4263683144be60053cb20d
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-11-06 10:35:11 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 1.74 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        
+        String[] arr = my_string.split("");
+        
+        // String[] arr = new String[my_string.length()];
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].equals(letter)) continue;
+            else answer += arr[i];
+            
+        }
+        
+        
+        return answer;
+    }
+}
