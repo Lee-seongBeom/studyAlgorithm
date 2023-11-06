2754d040710fe3db300e549ed98f96a1c97d6c4b
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-11-06 10:58:14 +0900

    [level unrated] Title: 수 조작하기 2, Time: 617.04 ms, Memory: 381 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        
+        for(int i=0; i<numLog.length; i++){
+            if(i >= numLog.length-1){
+                break;
+            }else{
+                int j = numLog[i+1] - numLog[i];
+                if(j == -1){
+                    answer += "s";
+                }else if(j == 1){
+                    answer += "w";
+                }else if(j == 10){
+                    answer += "d";
+                }else if(j == -10){
+                    answer += "a";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
