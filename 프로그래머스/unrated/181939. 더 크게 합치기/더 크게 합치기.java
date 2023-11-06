b122843ca773c4bd6975d5f5d96eb044bb22c734
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-11-06 10:46:08 +0900

    [level unrated] Title: 더 크게 합치기, Time: 1.67 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String ab = Integer.toString(a)+Integer.toString(b);
+        String ba = Integer.toString(b)+Integer.toString(a);
+        
+        int intAB = Integer.parseInt(ab);
+        int intBA = Integer.parseInt(ba);
+        
+        if(intAB > intBA){
+            answer = intAB;
+        }else{
+            answer = intBA;
+        }
+        
+        return answer;
+    }
+}
