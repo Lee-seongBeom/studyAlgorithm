027e6f879f9c0bf0fe2d664a0e55f64f0c4fe98a
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-11-06 10:36:06 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        while(true){
+            answer++;
+            if((slice*answer)/n >= 1){
+                break;
+            }
+            
+        }
+        
+        
+        
+        return answer;
+    }
+}
