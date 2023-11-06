296f1856ba61bb3bcecc1fbfc46d06de6bb0fbad
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-11-06 10:42:46 +0900

    [level unrated] Title: 카운트 업, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int start_num, int end_num) {
+        ArrayList answer = new ArrayList();
+        while(start_num <= end_num){
+            answer.add(start_num);
+            start_num++;
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
