bf2258b55b8a98c4894f72462018523e08fd9832
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2023-12-11 14:02:43 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int[] num_list, int n) {
+        ArrayList arr = new ArrayList();
+        
+        for(int i=0; i<num_list.length; i+=n){
+            arr.add(num_list[i]);
+        }
+        
+        return arr;
+    }
+}
