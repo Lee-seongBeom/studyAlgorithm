d2e27b851a1397d13bc53f426915c6781687f4b9
Lee-seongBeom <49877867+Lee-seongBeom@users.noreply.github.com>
2024-05-10 14:11:10 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        
+        answer[0] = array[0];
+        answer[1] = 0;  
+        
+        for(int i=1; i<array.length; i++){
+            if(array[i] > array[i-1]){
+                answer[0] = array[i];
+                answer[1] = i;    
+            }
+            
+        }
+        
+        
+        return answer;
+    }
+}
